efaad684136d569226130926eebdf4c00048460a
Alexander Sapko <sapcoreq@gmail.com>
2023-05-17 17:15:50 +0300

    Edited tasks 1, 2 and 3

--- 1_if1.py
+++ 1_if1.py
@@ -4,22 +4,49 @@
 
 Условный оператор: Возраст
 
-* Попросить пользователя ввести возраст при помощи input и положить 
+* Попросить пользователя ввести возраст при помощи input и положить
   результат в переменную
-* Написать функцию, которая по возрасту определит, чем должен заниматься пользователь: 
+* Написать функцию, которая по возрасту определит, чем должен заниматься пользователь:
   учиться в детском саду, школе, ВУЗе или работать
-* Вызвать функцию, передав ей возраст пользователя и положить результат 
+* Вызвать функцию, передав ей возраст пользователя и положить результат
   работы функции в переменную
 * Вывести содержимое переменной на экран
 
 """
+
+
+def what_should_you_do(user_age):
+    if user_age < 2 or user_age > 90:
+        return 'You should be at home'
+    elif user_age < 7:
+        return 'You should be at kindergarten'
+    elif user_age < 18:
+        return 'You should be at school'
+    elif user_age < 25:
+        return 'You should be at university'
+    else:
+        return 'You should be at work'
+
 
 def main():
     """
     Эта функция вызывается автоматически при запуске скрипта в консоли
     В ней надо заменить pass на ваш код
     """
-    pass
+    while True:
+        try:
+            age = float(input('How old are you? '))
+        except ValueError:
+            print('Please input a number')
+            continue
+        if age < 0 or age > 150:
+            print('Input realistic age')
+            continue
+        break
+
+    location = what_should_you_do(age)
+    print(location)
+
 
 if __name__ == "__main__":
     main()
--- 3_for.py
+++ 3_for.py
@@ -6,7 +6,7 @@
 
 * Дан список словарей с данными по колличеству проданных телефонов
   [
-    {'product': 'iPhone 12', 'items_sold': [363, 500, 224, 358, 480, 476, 470, 216, 270, 388, 312, 186]}, 
+    {'product': 'iPhone 12', 'items_sold': [363, 500, 224, 358, 480, 476, 470, 216, 270, 388, 312, 186]},
     {'product': 'Xiaomi Mi11', 'items_sold': [317, 267, 290, 431, 211, 354, 276, 526, 141, 453, 510, 316]},
     {'product': 'Samsung Galaxy 21', 'items_sold': [343, 390, 238, 437, 214, 494, 441, 518, 212, 288, 272, 247]},
   ]
@@ -16,12 +16,32 @@
 * Посчитать и вывести среднее количество продаж всех товаров
 """
 
+
 def main():
     """
     Эта функция вызывается автоматически при запуске скрипта в консоли
     В ней надо заменить pass на ваш код
     """
-    pass
-    
+    sales = [
+        {'product': 'iPhone 12', 'items_sold': [363, 500, 224, 358, 480, 476, 470, 216, 270, 388, 312, 186]},
+        {'product': 'Xiaomi Mi11', 'items_sold': [317, 267, 290, 431, 211, 354, 276, 526, 141, 453, 510, 316]},
+        {'product': 'Samsung Galaxy 21', 'items_sold': [343, 390, 238, 437, 214, 494, 441, 518, 212, 288, 272, 247]},
+    ]
+    overall_total_sales = 0
+    overall_sales_qty = 0
+    for sale in sales:
+        total_sales = sum(sale['items_sold'])
+        sales_qty = len(sale['items_sold'])
+        print(sale['product'])
+        print(f'Total sales: {total_sales}')
+        print(f'Average sales: {round(total_sales / sales_qty, 1)}')
+        overall_total_sales += total_sales
+        overall_sales_qty += sales_qty
+        print()
+
+    print(f'Overall total sales: {overall_total_sales}')
+    print(f'Overall average sales: {round(overall_total_sales / overall_sales_qty, 1)}')
+
+
 if __name__ == "__main__":
     main()
